9c35028c2ead3c48a2e2e4cfc14998402ef6e3fc
Stella Gos <stella.gospodinova@sohonet.com>
2026-02-10 15:29:57 +0200

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt", "r") as fs:
 
 setup(
     name="napalm-arubaos-cli",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     author="David Barroso",
     author_email="dbarrosop@dravetech.com",
